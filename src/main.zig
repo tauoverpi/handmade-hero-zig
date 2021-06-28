5461df9bc4d3c22eee2c42d5fcba59753a97d220
Tau <tauoverpi@yandex.com>
2021-06-29 01:47:34 +0200

    Day 4: animating the backbuffer

--- src/main.zig
+++ src/main.zig
@@ -8,30 +8,47 @@ const WINAPI = @import("std").os.windows.WINAPI;
 usingnamespace @import("win32").zig;
 usingnamespace @import("win32").foundation;
 usingnamespace @import("win32").system.system_services;
+usingnamespace @import("win32").system.memory;
 usingnamespace @import("win32").ui.windows_and_messaging;
 usingnamespace @import("win32").graphics.gdi;
 
 var running: bool = true;
-var bitmap_memory: *c_void = undefined;
+var bitmap_memory: ?[*]u8 = null;
 var bitmap_info = mem.zeroes(BITMAPINFO);
 var bitmap_handle: ?HBITMAP = null;
 var device_context: HDC = undefined;
+var bitmap_width: i32 = undefined;
+var bitmap_height: i32 = undefined;
+
+fn renderOddGradient(x_offset: u32, y_offset: u32) void {
+    _ = x_offset;
+    _ = y_offset;
+    const pitch = @intCast(u32, bitmap_width) * 4;
+    var y: u32 = 0;
+    var row = bitmap_memory.?;
+    while (y < bitmap_height) : (y += 1) {
+        var x: u32 = 0;
+        var pixel = @ptrCast([*]u32, @alignCast(4, row));
+        while (x < bitmap_width) : (x += 1) {
+            pixel.* = (x +% x_offset) | ((y +% y_offset) << 8);
+            pixel += 1;
+        }
+        row += pitch;
+    }
+}
 
 fn resizeDibSection(width: i32, height: i32) void {
-    _ = width;
-    _ = height;
-
-    if (bitmap_handle) |handle| {
-        _ = DeleteObject(handle);
-    } else {
-        // TODO: check
-        device_context = CreateCompatibleDC(null);
+    if (bitmap_memory) |memory| {
+        _ = VirtualFree(@ptrCast(*c_void, memory), 0, MEM_RELEASE);
     }
+
+    bitmap_height = height;
+    bitmap_width = width;
 
     bitmap_info.bmiHeader = .{
         .biSize = @sizeOf(@TypeOf(bitmap_info.bmiHeader)),
         .biWidth = width,
-        .biHeight = height,
+        .biHeight = -height,
         .biPlanes = 1,
         .biBitCount = 32,
         .biCompression = BI_RGB,
@@ -42,29 +59,33 @@ fn resizeDibSection(width: i32, height: i32) void {
         .biClrImportant = 0,
     };
 
-    bitmap_handle = CreateDIBSection(
-        device_context,
-        &bitmap_info,
-        DIB_RGB_COLORS,
-        &bitmap_memory,
-        null,
-        0,
-    );
+    const size = @intCast(usize, bitmap_width) * @intCast(usize, bitmap_height) * 4;
+    bitmap_memory = @ptrCast([*]u8, VirtualAlloc(null, size, MEM_COMMIT, PAGE_READWRITE));
+
+    // TODO: clear
 }
 
-fn updateWindow(ctx: HDC, x: i32, y: i32, width: i32, height: i32) void {
+fn updateWindow(ctx: HDC, rect: RECT, x: i32, y: i32, width: i32, height: i32) void {
+    _ = x; // TODO: use
+    _ = y; // TODO: use
+    _ = width; // TODO: use
+    _ = height; // TODO: use
+
+    const wh = rect.bottom - rect.top;
+    const ww = rect.right - rect.left;
+
     _ = StretchDIBits(
         ctx,
         // dest rect
-        x,
-        y,
-        width,
-        height,
+        0, //x,
+        0, //y,
+        bitmap_width,
+        bitmap_height,
         // source rect
-        x,
-        y,
-        width,
-        height,
+        0, //x,
+        0, //y,
+        ww,
+        wh,
         bitmap_memory,
         &bitmap_info,
         DIB_RGB_COLORS,
@@ -122,8 +143,10 @@ fn mainWindowCallback(
             const height = paint.rcPaint.bottom - paint.rcPaint.top;
             const width = paint.rcPaint.right - paint.rcPaint.left;
 
-            updateWindow(ctx, x, y, width, height);
-            _ = ctx;
+            var client_rect: RECT = undefined;
+            _ = GetClientRect(window, &client_rect);
+
+            updateWindow(ctx, client_rect, x, y, width, height);
         },
         else => {
             result = DefWindowProcA(window, message, wparam, lparam);
@@ -169,10 +192,29 @@ pub fn wWinMain(
 
     var msg: MSG = undefined;
 
-    while (running) if (GetMessageA(&msg, null, 0, 0) > 0) {
-        _ = TranslateMessage(&msg);
-        _ = DispatchMessageA(&msg);
-    } else break;
+    var x_offset: u32 = 0;
+    var y_offset: u32 = 0;
+    while (running) {
+        while (PeekMessageA(&msg, null, 0, 0, PM_REMOVE) != 0) {
+            if (msg.message == WM_QUIT) running = false;
+            _ = TranslateMessage(&msg);
+            _ = DispatchMessageA(&msg);
+        }
+
+        renderOddGradient(x_offset, y_offset);
+        x_offset += 1;
+
+        const ctx = GetDC(window);
+        defer _ = ReleaseDC(window, ctx);
+
+        var client_rect: RECT = undefined;
+        _ = GetClientRect(window, &client_rect);
+
+        const height = client_rect.bottom - client_rect.top;
+        const width = client_rect.right - client_rect.left;
+
+        updateWindow(ctx, client_rect, 0, 0, width, height);
+    }
 
     return 0;
 }
